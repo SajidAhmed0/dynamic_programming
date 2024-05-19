547e6549d7e959d2efa5d6201e1742e9a78f9c87
SajidAhmed0 <mjsahmed205@gmail.com>
2024-05-19 21:54:52 +0530

    added memoizatoin for fib function

--- fibonacci/Main.java
+++ fibonacci/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("hello world");
-        System.out.println(fib(50));
+        System.out.println(fib(50, new long[50]));
     }
 
-    public static int fib(int n) {
-        if (n <= 2) {
-            return 1;
+    public static long fib(int n, long[] intArray) {
+        if (intArray[n - 1] != 0) {
+            // System.out.println(intArray[n - 1]);
+            return intArray[n - 1];
         } else {
-            return fib(n - 1) + fib(n - 2);
+            long result;
+            if (n <= 2) {
+                result = 1;
+                intArray[n - 1] = result;
+            } else {
+                result = fib(n - 1, intArray) + fib(n - 2, intArray);
+                intArray[n - 1] = result;
+            }
+            // System.out.println(result);
+            return result;
         }
+
     }
 }
