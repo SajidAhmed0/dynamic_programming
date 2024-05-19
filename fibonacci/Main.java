1200baf9842838a3d20cfb68ca0819b5ce2d1ba7
SajidAhmed0 <mjsahmed205@gmail.com>
2024-05-19 21:38:17 +0530

    fib added

--- fibonacci/Main.java
+++ fibonacci/Main.java
@@ -1,5 +1,14 @@
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String args[]) {
         System.out.println("hello world");
+        System.out.println(fib(50));
+    }
+
+    public static int fib(int n) {
+        if (n <= 2) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
     }
 }
