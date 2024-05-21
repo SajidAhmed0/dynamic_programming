e905a09c416216b0b754e6fb6e0f9e72e21ae39e
SajidAhmed0 <mjsahmed205@gmail.com>
2024-05-21 19:54:44 +0530

    memoization added

--- minimumCoin/minCoin.java
+++ minimumCoin/minCoin.java
@@ -1,32 +1,39 @@
 package minimumCoin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinCoin {
 
     public static void main(String args[]) {
-        Integer[] coins = { 9, 10, 20, 5 };
-        Integer m = 45;
+        Integer[] coins = { 1, 4, 5 };
+        Integer m = 150;
 
         MinCoin minCoin = new MinCoin();
 
-        System.out.println(minCoin.minimum_coins(m, coins));
+        System.out.println(minCoin.minimum_coins(m, coins, new HashMap<Integer, Integer>()));
 
     }
 
-    public Integer minimum_coins(Integer m, Integer[] coins) {
+    public Integer minimum_coins(Integer m, Integer[] coins, Map<Integer, Integer> memo) {
         Integer answer;
         if (m == 0)
             answer = 0;
-        else {
+
+        else if (memo.containsKey(m)) {
+            return memo.get(m);
+        } else {
             answer = null;
             for (Integer coin : coins) {
                 Integer subProblem = m - coin;
                 if (subProblem < 0)
                     continue;
-                Integer subAns = minimum_coins(subProblem, coins);
-                if(subAns != null)
+                Integer subAns = minimum_coins(subProblem, coins, memo);
+                if (subAns != null)
                     answer = min_ignore_null(answer, subAns + 1);
             }
         }
+        memo.put(m, answer);
         return answer;
     }
 
