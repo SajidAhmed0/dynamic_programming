b6ec4b6261f7a1bdb49dfc38c298a983cd1dc4f3
SajidAhmed0 <mjsahmed205@gmail.com>
2024-05-21 20:11:40 +0530

    bottom up for fib

--- fibonacci/Main.java
+++ fibonacci/Main.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String args[]) {
         System.out.println("hello world");
         System.out.println(fib(50, new long[50]));
+        System.out.println(fibBottomUp(50));
     }
 
     public static long fib(int n, long[] intArray) {
@@ -21,5 +25,20 @@ public class Main {
             return result;
         }
 
+    }
+
+    public static Long fibBottomUp(int n) {
+        Map<Long, Long> memo = new HashMap<>();
+
+        for (int i = 1; i <= n; i++) {
+            Long result;
+            if (i <= 2)
+                result = (long) 1;
+            else {
+                result = memo.get((long) i - 1) + memo.get((long) i - 2);
+            }
+            memo.put((long) i, result);
+        }
+        return memo.get((long) n);
     }
 }
