3f1336e9b727330f4cfc68aa2af0b3e632b9152f
SajidAhmed0 <mjsahmed205@gmail.com>
2024-05-21 19:30:38 +0530

    minimum_coins finished

--- minimumCoin/minCoin.java
+++ minimumCoin/minCoin.java
@@ -1,6 +1,27 @@
 package minimumCoin;
 
 public class minCoin {
+
+    public static void main(String args[]) {
+
+    }
+
+    public Integer minimum_coins(Integer m, Integer[] coins) {
+        Integer answer;
+        if (m == 0)
+            answer = 0;
+        else {
+            answer = null;
+            for (Integer coin : coins) {
+                Integer subProblem = m - coin;
+                if (subProblem < 0)
+                    continue;
+                answer = min_ignore_null(answer, minimum_coins(subProblem, coins) + 1);
+            }
+        }
+        return answer;
+    }
+
     private Integer min_ignore_null(Integer a, Integer b) {
         if (a == null)
             return b;
